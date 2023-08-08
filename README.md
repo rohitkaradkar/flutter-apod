c6829458a0f5d712a1cdc0df0d5d8bc4f8cd722a
Rohit Karadkar <karadkar.rohit@gmail.com>
2023-08-08 12:03:55 +0530

    Add Json Model for API Response (#6)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # APOD
+
+### Instructions
+
+Get dependencies and generate `*.g.dart` files
+
+```bash
+flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
+```
---
+++ test/picture_response_test.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:apod/picture_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('picture response json decoding', () {
+    const json = """
+    {
+    "copyright": "\\nAbe Jones\\n",
+    "date": "2023-08-07",
+    "explanation": "Some explanation",
+    "hdurl": "https://apod.nasa.gov/apod/image/2308/LDN935_Jones_1840.jpg",
+    "title": "The Pelican Nebula in Gas, Dust, and Stars",
+    "url": "https://apod.nasa.gov/apod/image/2308/LDN935_Jones_960.jpg"
+    }
+    """;
+    final response = PictureResponse.fromJson(
+      jsonDecode(json.replaceAll("\\n", "")),
+    );
+    final expected = PictureResponse(
+      date: DateTime(2023, 8, 7),
+      explanation: "Some explanation",
+      hdImageUrl: "https://apod.nasa.gov/apod/image/2308/LDN935_Jones_1840.jpg",
+      imageUrl: "https://apod.nasa.gov/apod/image/2308/LDN935_Jones_960.jpg",
+      title: "The Pelican Nebula in Gas, Dust, and Stars",
+      copyright: "Abe Jones",
+    );
+
+    expect(response.date, expected.date);
+    expect(response.explanation, expected.explanation);
+    expect(response.hdImageUrl, expected.hdImageUrl);
+    expect(response.imageUrl, expected.imageUrl);
+    expect(response.title, expected.title);
+    expect(response.copyright, expected.copyright);
+  });
+}
