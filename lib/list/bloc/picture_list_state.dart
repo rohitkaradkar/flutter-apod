c60c3cc3790d626e337135ea122ba3fc84a66084
Rohit Karadkar <karadkar.rohit@gmail.com>
2023-08-17 15:26:56 +0530

    Fix pagination to avoid fetching duplicate data (#17)

--- lib/list/bloc/picture_list_state.dart
+++ lib/list/bloc/picture_list_state.dart
@@ -6,26 +6,22 @@ enum PictureListStatus { loading, success, error }
 final class PictureListState extends Equatable {
   final PictureListStatus status;
   final List<PictureItem> pictures;
-  final int pageIndex;
 
   const PictureListState({
     this.status = PictureListStatus.loading,
     this.pictures = const <PictureItem>[],
-    this.pageIndex = -1,
   });
 
   PictureListState copyWith({
     PictureListStatus? status,
     List<PictureItem>? pictures,
-    int? pageIndex,
   }) {
     return PictureListState(
       status: status ?? this.status,
       pictures: pictures ?? this.pictures,
-      pageIndex: pageIndex ?? this.pageIndex,
     );
   }
 
   @override
-  List<Object> get props => [status, pictures, pageIndex];
+  List<Object> get props => [status, pictures];
 }
