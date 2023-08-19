264b4671984a1cc8a8544eabaf2c8c808a783469
Rohit Karadkar <karadkar.rohit@gmail.com>
2023-08-19 10:40:43 +0530

    Move Detail Page view to new screen

--- lib/details/picture_details_page.dart
+++ lib/details/picture_details_page.dart
@@ -1,8 +1,7 @@
 import 'package:apod/data/picture_repository.dart';
 import 'package:apod/details/bloc/picture_details_bloc.dart';
-import 'package:apod/details/model/picture_detail_item.dart';
+import 'package:apod/details/widget/picture_details_page_view.dart';
 import 'package:apod/utils/constants.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -27,81 +26,6 @@ class PictureDetailsPage extends StatelessWidget {
           ),
         ),
         body: const PictureDetailsPageView(),
-      ),
-    );
-  }
-}
-
-class PictureDetailsPageView extends StatefulWidget {
-  const PictureDetailsPageView({
-    super.key,
-  });
-
-  @override
-  State<PictureDetailsPageView> createState() => _PictureDetailsPageViewState();
-}
-
-class _PictureDetailsPageViewState extends State<PictureDetailsPageView> {
-  PageController? _pageController;
-
-  @override
-  void dispose() {
-    _pageController?.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<PictureDetailsBloc, PictureDetailsState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
-      builder: (context, state) {
-        if (state.pictures.isEmpty) {
-          if (state.status == PictureDetailsStatus.loading) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            return const Center(child: Text('No pictures found'));
-          }
-        }
-        final initialPageIndex = state.selectedPictureIndex;
-        if (initialPageIndex >= 0) {
-          _pageController = PageController(initialPage: initialPageIndex);
-        }
-        return PageView.builder(
-          itemCount: state.pictures.length,
-          controller: _pageController,
-          itemBuilder: (context, index) {
-            final picture = state.pictures[index];
-            // container with width of screen
-            return _imagePage(picture);
-          },
-        );
-      },
-    );
-  }
-
-  Widget _imagePage(PictureDetailItem picture) {
-    return CachedNetworkImage(
-      imageUrl: picture.imageUrl,
-      progressIndicatorBuilder: (context, url, downloadProgress) {
-        return _progressIndicator(downloadProgress);
-      },
-    );
-  }
-
-  Widget _progressIndicator(DownloadProgress downloadProgress) {
-    final children = <Widget>[const CircularProgressIndicator()];
-    if (downloadProgress.progress != null) {
-      children.addAll([
-        const SizedBox(height: 4),
-        Text('${(downloadProgress.progress! * 100).round()}%')
-      ]);
-    }
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: children,
       ),
     );
   }
