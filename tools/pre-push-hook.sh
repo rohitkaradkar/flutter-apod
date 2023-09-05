71647a44203604a2819cf4bcae7bfa606cbdaf7a
Rohit Karadkar <karadkar.rohit@gmail.com>
2023-09-05 14:43:51 +0530

    Add pre push hook

---
+++ tools/pre-push-hook.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+hookFile='.git/hooks/pre-push'
+gitDir='.git'
+
+# function that checks if previous command was successful else exits
+function appendCheckSuccess() {
+  # shellcheck disable=SC2028
+  echo "if [ \$? -ne 0 ]; then\\n exit 1\\nfi" >> $hookFile
+}
+
+if [ ! -d $gitDir ]
+then
+  echo 'git not initialised yet'
+  exit 1
+elif [ ! -f $hookFile ]
+then
+  echo 'Installing flutter test'
+  echo 'flutter test' >> $hookFile
+  appendCheckSuccess
+
+  echo 'Installing dart formatting'
+  echo 'dart format -o none --set-exit-if-changed .' >> $hookFile
+  appendCheckSuccess
+else
+  echo 'pre-push hook already present'
+  echo 'check .git/hooks/pre-push for more details'
+fi
